e74cc99d2b2c79638d2a97d6c9e95986fcde35c2
MADMechaniculus <rihanna271298@gmail.com>
2022-02-03 00:28:56 +0300

    Add defines for implementation and declaration of interface methods

--- defines.h
+++ defines.h
@@ -1,6 +1,42 @@
 #ifndef DEFINES_H
 #define DEFINES_H
 
-#define INTERFACE_METHOD(method, out_construction, input_construction)
+#include <string>
+
+#define INTERFACE_SET_IMPL(classname, method, input_t) \
+    std::string method::name = ""#method""; \
+    std::string method::type = "interface_getter"; \
+    void classname::method::__call(input_t & input)
+
+#define INTERFACE_GET_IMPL(classname, method, output_t) \
+    std::string method::name = ""#method""; \
+    std::string method::type = "interface_getter"; \
+    void classname::method::__call(output_t & output)
+
+#define INTERFACE_IMPL(classname, method, input_t, output_t) \
+    std::string method::name = ""#method""; \
+    std::string method::type = "interface_getter"; \
+    void classname::method::__call(input_t & input, output_t & output)
+
+#define INTERFACE_BODY(method) \
+    class method { \
+    public: \
+        static std::string name; \
+        static std::string type;
+
+#define INTERFACE_METHOD_DECL(method, out_construction, input_construction) \
+    INTERFACE_BODY(method); \
+    static void __call(input_construction & input, out_construction & output); \
+};
+
+#define INTERFACE_GET_DECL(method, out_construction) \
+    INTERFACE_BODY(method) \
+    static void __call(out_construction & output); \
+};
+
+#define INTERFACE_SET_DECL(method, input_construction) \
+    INTERFACE_BODY(method) \
+    static void __call(input_construction & input); \
+};
 
 #endif // DEFINES_H
--- eventloopapplication.h
+++ eventloopapplication.h
@@ -2,23 +2,52 @@
 #define EVENTLOOPAPPLICATION_H
 
 #include "abstractapplication.h"
+#include "defines.h"
+
+#include <boost/function.hpp>
+
+#include <queue>
+
+/**
+ * @brief Очередь задач для приложения, поступающих извне
+ */
+typedef std::queue<boost::function<void()>> taskQueue_t;
 
 typedef struct {
 
+} appDescription_t;
+
+typedef struct {
+    // Some params to prepare application in init function
 } initParams_t;
 
 typedef struct {
-
+    // Some params to put in process function
 } processParams_t;
 
 typedef struct {
-
+    // Some params to put in stop function
 } stopParams_t;
 
+// Пример приложения, использующего очередь задач для внешнего http сервера
 class EventLoopApplication : public AbstractApplication<initParams_t, processParams_t, stopParams_t>
 {
+    taskQueue_t internatlTaskQueue;
+
 public:
     EventLoopApplication(int argc, char * argv[]) : AbstractApplication<initParams_t, processParams_t, stopParams_t>(argc, argv) { }
+
+    INTERFACE_GET_DECL(getAppDescription, appDescription_t);
+
+    bool pullTask(std::function<void()> && task) {
+        if (this->internatlTaskQueue.size() < 10) {
+            this->internatlTaskQueue.push(task);
+            return true;
+        } else {
+            // Task queue overflowed
+            return false;
+        }
+    }
 
     bool init(initParams_t & params) {
         (void)params;
@@ -31,6 +60,16 @@ public:
         // Infinite loop
         while (true) {
             // main application procedures
+            // Some hard work
+
+            /// Это решение в лоб :) (вряд ли правильное и эффективное)
+            /// Задачи поступаемые от http сервера имеют самый низкий приоритет, поэтому на стороне
+            /// клиента необходимо предусмотреть ожидание ответа от сервера, потому как это может занять
+            /// много времени (пока приоритетные задачи будут выполнены)
+            if (!internatlTaskQueue.empty()) {
+                internatlTaskQueue.front()();
+                internatlTaskQueue.pop();
+            }
         }
 
         return true;
@@ -41,5 +80,9 @@ public:
         return true;
     }
 };
+
+INTERFACE_GET_IMPL(EventLoopApplication, getAppDescription, appDescription_t) {
+
+}
 
 #endif // EVENTLOOPAPPLICATION_H
